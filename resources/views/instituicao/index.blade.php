3ca0ed76e32462b0359a6ab6bd04c1bab50ed538
WillamisRodrigues <wrodrigues153@gmail.com>
2019-06-18 20:14:50 -0300

    lista de instituição

--- resources/views/instituicao/index.blade.php
+++ resources/views/instituicao/index.blade.php
@@ -1,209 +1,68 @@
 @extends('layout/app')
-@section('titulo','Cadastro Instituição | RH MAIS')
+@section('titulo','Instituição de Ensino | RH MAIS')
 @section('conteudo')
-    <div class="container body">
+   <div class="container body">
       <div class="main_container">
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
-          @include('layout.menu.menu')
-            <!-- /menu profile quick info -->
-
+            @include('layout.menu.menu')
             <br />
             @include('layout.menu.sidebar')
-            <!-- /sidebar menu -->
           </div>
         </div>
-        @include('layout.menu.menutop')
+            @include('layout.menu.menutop')
         <!-- page content -->
-        <div class="right_col" role="main">
+          <!-- page content -->
+          <div class="right_col" role="main">
+          <div class="">
+
             <div class="clearfix"></div>
 
             <div class="row">
-
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Cadastro de Instituição</h2>
+                  <a href="" class="btn btn-success pull-right"> <i class="fa fa-home"> </i> Adicionar Instituição de Ensino</a>
+                    <h2>Instituição de Ensino</h2>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                  <div id="wizard" class="form_wizard wizard_horizontal">
-                      <ul class="wizard_steps">
-                        <li>
-                          <a href="#step-1">
-                            <span class="step_no">1</span>
-                            <span class="step_descr">
-                                              Cadastro Empresa<br />
-                                              <small>Dados da Empresa</small>
-                                          </span>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#step-2">
-                            <span class="step_no">2</span>
-                            <span class="step_descr">
-                                              Cadastro de Endereço<br />
-                                              <small>Dados do Endereço</small>
-                                          </span>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#step-3">
-                            <span class="step_no">3</span>
-                            <span class="step_descr">
-                                              Cadastro Responsável<br />
-                                              <small>Dados do Responsável</small>
-                                          </span>
-                          </a>
-                        </li>
-                      </ul>
-                      <div id="step-1" style="overflow:hidden;">
-                      <form class="form-horizontal form-label-left input_mask">
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="CNPJ">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
+                    <table id="usuario" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Nome</th>
+                          <th>Complemento</th>
+                          <th>Cidade</th>
+                          <th>Endereço</th>
+                          <th>CNPJ</th>
+                          <th>Ação</th>
+                        </tr>
+                      </thead>
 
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Razão Social">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Complemento">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
 
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Mantenedora">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Insc. Estadual">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Endereço Site">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-
-                      </div>
-                      <div id="step-2">
-                      <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Estado">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Cidade">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Bairro">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Endereço">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Complemento">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Numero">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="CEP">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Telefone">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                      </div>
-                      <div id="step-3">
-                      <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Nome do Contato">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Email do Contato">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Celular Contato">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Celular Representante">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Email do Contato">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="Email Representante">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        <div class="row" style="width:960px; margin: 0 auto;">
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="CPF Representante">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                          </div>
-
-                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                            <input type="text" class="form-control" id="inputSuccess3" placeholder="RG Represetante">
-                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
-                          </div>
-                        </div>
-                        </form>
-                      </div>
-                    </div>
-                    <!-- End SmartWizard Content -->
+                      <tbody>
+                        <tr>
+                          <td>'CAIXA ESCOLAR MARCOS BORGES DE MIRANDA</td>
+                          <td></td>
+                          <td>UBERLANDIA</td>
+                          <td>AV. COMENDADOR ALEXANDRINO GARCIA	</td>
+                          <td>21.296.223/0001-18
+                          </td>
+                          <td><a href="" class="btn btn-primary"> <i class="fa fa-plus"> </i> Editar</a></td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
         </div>
+    </div>
+    </div>
         <!-- /page content -->
 
         <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
+        @include('layout.footer')
         <!-- /footer content -->
       </div>
     </div>
