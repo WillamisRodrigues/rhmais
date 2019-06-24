cb7d6ea4b291fd3691d2e93630bcfab889cf0a2f
WillamisRodrigues <wrodrigues153@gmail.com>
2019-06-24 20:13:12 -0300

    tela de cadastro estagiario

--- resources/views/estagiario/create.blade.php
+++ resources/views/estagiario/create.blade.php
@@ -23,7 +23,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Cadastro de Usuário</h2>
+                    <h2>Cadastro de Estagiário</h2>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
@@ -33,9 +33,11 @@
                       <!-- SmartWizard html -->
                       <div id="smartwizard">
                           <ul>
-                              <li><a href="#step-1">Passo 1<br /><small>Dados Pessoais</small></a></li>
+                          <li><a href="#step-1">Passo 1<br /><small>Dados Pessoais</small></a></li>
                               <li><a href="#step-2">Passo 2<br /><small>Dados de Endereço</small></a></li>
                               <li><a href="#step-3">Passo 3<br /><small>Dados da Empresa</small></a></li>
+                              <li><a href="#step-4">Passo 4<br /><small>Escolaridade</small></a></li>
+                              <li><a href="#step-5">Passo 5<br /><small>Outras informações</small></a></li>
                           </ul>
 
                           <div>
@@ -137,6 +139,84 @@
                                     </div>
                                     </div>
                                 </div>
+                              </div>
+                              <div id="step-4">
+                                    <div id="form-step-0" role="form" data-toggle="validator">
+                                    <div class="row" style="width:960px; margin: 20px auto;">
+                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <select class="form-control has-feedback-left" name="nivel">
+                                                    <option>Selecione um nível</option>
+                                                </select>
+                                                <span class="fa fa-graduation-cap form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <select class="form-control has-feedback-left" name="curso">
+                                                    <option>Selecione um curso</option>
+                                                </select>
+                                                <span class="fa fa-graduation-cap form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <select class="form-control has-feedback-left" name="curso">
+                                                    <option>Período</option>
+                                                </select>
+                                                <span class="fa fa-graduation-cap form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <select class="form-control has-feedback-left" name="horario">
+                                                    <option>Horário de Estudo</option>
+                                                </select>
+                                                <span class="fa fa-clock-o form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <select class="form-control has-feedback-left" name="instituicao_ensino">
+                                                    <option>Instituição de Ensino</option>
+                                                </select>
+                                                <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <input type="text" class="form-control has-feedback-left" placeholder="Matricula" name="matricula">
+                                                <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <input type="text" class="form-control has-feedback-left" placeholder="Previsão Término Curso" name="previsao_termino">
+                                                <span class="fa fa-book form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                              </div>
+                              <div id="step-5">
+                                    <div id="form-step-0" role="form" data-toggle="validator">
+                                        <div class="row" style="width:960px; margin: 20px auto;">
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <input type="text" class="form-control has-feedback-left" placeholder="Banco/Agência" name="banco_agencia">
+                                                <span class="fa fa-bank form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <input type="text" class="form-control has-feedback-left" placeholder="Conta" name="conta">
+                                                <span class="fa fa-bars form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <input type="text" class="form-control has-feedback-left" placeholder="Código da vaga" name="codigo_vaga">
+                                                <span class="fa fa-barcode form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <input type="text" class="form-control has-feedback-left" placeholder="Criar Senha" name="criar_senha">
+                                                <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                                <div class="checkbox">
+                                                    <label>
+                                                    <input type="checkbox" class="flat"> Esta Ativo
+                                                    </label>
+                                                </div>    
+                                            </div>
+                                            <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+                                                <textarea class="form-control" placeholder="Sua observação" name="criar_senha">
+                                                </textarea>
+                                            </div>
+                                        </div>
+                                    </div>
                               </div>
                           </div>
                       </div>
