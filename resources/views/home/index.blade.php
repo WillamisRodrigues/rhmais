0390892802dc797f6a27a496a92a0ba3137b2071
WillamisRodrigues <wrodrigues153@gmail.com>
2019-06-24 21:34:52 -0300

    ajustes tela home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -34,7 +34,7 @@
           <!-- /top tiles -->
             <div class="clearfix"></div>
             <div class="row">
-              <div class="col-md-4 col-sm-4 col-xs-12">
+              <div class="col-md-6 col-sm-6 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>RH TALENTOS | SEXO</h2>
@@ -48,10 +48,10 @@
                 </div>
               </div>
 
-              <div class="col-md-4 col-sm-4 col-xs-12">
+              <div class="col-md-6 col-sm-6 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>RH MAIS TALENTOS | SETOR</h2>
+                    <h2>RH MAIS TALENTOS | ESCOLARIDADE</h2>
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                       </li>
@@ -73,33 +73,6 @@
 
                     <div id="echart_pie2" style="height:350px;"></div>
 
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 col-sm-4 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Outros</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <div id="echart_donut" style="height:350px;"></div>
                   </div>
                 </div>
               </div>
