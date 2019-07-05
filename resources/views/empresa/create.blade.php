e3454a90071d787a9cecd6ff0c56a0b829a57b66
WillamisRodrigues <wrodrigues153@gmail.com>
2019-07-04 21:45:43 -0300

    correcao de alertas instituição e empresa

--- resources/views/empresa/create.blade.php
+++ resources/views/empresa/create.blade.php
@@ -21,6 +21,7 @@
             <div class="row">
 
               <div class="col-md-12 col-sm-12 col-xs-12">
+              @include('layout.alerta.flash-message')
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>Cadastro de Empresas</h2>
