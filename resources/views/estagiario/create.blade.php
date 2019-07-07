df00621547dd012e08369b1f3a7e1af2372eb5d3
WillamisRodrigues <wrodrigues153@gmail.com>
2019-07-07 10:44:36 -0300

    cadastro estagiario

--- resources/views/estagiario/create.blade.php
+++ resources/views/estagiario/create.blade.php
@@ -1,182 +1,70 @@
 @extends('layout/app')
-@section('titulo','Editar Estagiário | RH MAIS')
+@section('titulo','Cadastro Empresas | RH MAIS')
 @section('conteudo')
     <div class="container body">
       <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+          @include('layout.menu.menu')
+            <!-- /menu profile quick info -->
+
+            <br />
+            @include('layout.menu.sidebar')
+            <!-- /sidebar menu -->
+          </div>
+        </div>
+        @include('layout.menu.menutop')
+        <!-- page content -->
+        <div class="right_col" role="main">
             <div class="clearfix"></div>
 
             <div class="row">
 
               <div class="col-md-12 col-sm-12 col-xs-12">
+              @include('layout.alerta.flash-message')
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Editar Estagiário</h2>
+                    <h2>Cadastro de Empresas</h2>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                  <form action="{{ route('estagiario.update',  $estagiario->id) }}" id="myForm" role="form" data-toggle="validator" method="post" accept-charset="utf-8">
-                   <input type="hidden" name="_method" value="PUT">
-                  <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                  <form action="{{ route('estagiario.store') }}" id="myForm" role="form" data-toggle="validator" method="post" accept-charset="utf-8">
+                  {{csrf_field()}}
+
                       <!-- SmartWizard html -->
-                      <div>
+                      <div id="smartwizard">
+                          <ul>
+                              <li><a href="#step-1">Passo 1<br /><small>Cadastro Empresa</small></a></li>
+                              <li><a href="#step-2">Passo 2<br /><small>Cadastro de Endereço</small></a></li>
+                          </ul>
+
                           <div>
-                              <div>
+                              <div id="step-1">
                                     <br>
                                   <div id="form-step-0" role="form" data-toggle="validator">
-                                  <div class="row" style="width:960px; margin: 0 auto;">
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" value="{{ $estagiario->nome }}" class="form-control has-feedback-left" placeholder="Nome Completo" name="nome">
-                                        <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-
-                                     <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" value="{{ $estagiario->email }}" class="form-control has-feedback-left" placeholder="Email" name="email">
-                                        <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                    </div>
-                                    <div class="row" style="width:960px; margin: 0 auto;">
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" value="{{ $estagiario->rg }}" maxlength="12" class="form-control has-feedback-left" placeholder="RG" name="rg">
-                                        <span class="fa fa-newspaper-o form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" value="{{ $estagiario->cpf }}" maxlength="14" class="form-control has-feedback-left"  placeholder="CPF" name="cpf">
-                                        <span class="fa fa-newspaper-o form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                    </div>
-                                    <div class="row" style="width:960px; margin: 0 auto;">
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" value="{{ $estagiario->telefone }}" class="form-control has-feedback-left" placeholder="Telefone" name="telefone">
-                                        <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" value="{{ $estagiario->celular }}" class="form-control has-feedback-left" placeholder="Celular" name="celular">
-                                        <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                    </div>
-                                    <div class="row" style="width:960px; margin: 0 auto;">
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="date" value="{{ $estagiario->data_nascimento }}" class="form-control has-feedback-left" placeholder="Data de Nascimento" name="data_nascimento">
-                                        <span class="fa fa-calendar form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                            <input type="text" value="{{ $estagiario->bairro }}" class="form-control has-feedback-left" placeholder="Bairro" name="bairro">
-                                            <span class="fa fa-map-marker form-control-feedback left" aria-hidden="true"></span>
-                                        </div>
-                                    </div>
-                                    <div class="row" style="width:960px; margin: 0 auto;">
-
-                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                            <input type="text" value="{{ $estagiario->endereco }}" class="form-control has-feedback-left" placeholder="Endereço" name="endereco">
-                                            <span class="fa fa-map-marker form-control-feedback left" aria-hidden="true"></span>
-                                        </div>
-                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                            <input type="text" value="{{ $estagiario->complemento }}" maxlength="5" class="form-control has-feedback-left" placeholder="Complemento" name="complemento">
-                                            <span class="fa fa-map-marker form-control-feedback left" aria-hidden="true"></span>
-                                        </div>
-
-
-                                        </div>
-                                    <div class="row" style="width:960px; margin: 0 auto;">
-
-                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                            <input type="text" value="{{ $estagiario->numero }}" class="form-control has-feedback-left" placeholder="Numero" name="numero">
-                                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                                        </div>
-                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                            <input type="text" value="{{ $estagiario->estado }}" class="form-control has-feedback-left" id="inputSuccess4" placeholder="Estado" name="estado">
-                                            <span class="fa fa-newspaper-o form-control-feedback left" aria-hidden="true"></span>
-                                        </div>
-                                        </div>
-                                        <div class="row" style="width:960px; margin: 0 auto;">
-
-                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                            <input type="text" value="{{ $estagiario->cidade }}" class="form-control has-feedback-left" placeholder="Cidade" name="cidade">
-                                            <span class="fa fa-newspaper-o form-control-feedback left" aria-hidden="true"></span>
-                                        </div>
-                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" value="{{ $estagiario->unid_concendente }}" class="form-control has-feedback-left" placeholder="Unidade Concedente" name="und_condente">
-                                        <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                        </div>
-                                        <div class="row" style="width:960px; margin: 0 auto;">
-
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                        <input type="text" class="form-control has-feedback-left" value="RH Mais" readonly placeholder="Agente de Integração" name="agenteint">
-                                        <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                    <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <select class="form-control has-feedback-left" name="nivel">
-                                                    <option>Selecione um nível</option>
-                                                </select>
-                                                <span class="fa fa-graduation-cap form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <select class="form-control has-feedback-left" name="curso">
-                                                    <option>Período</option>
-                                                </select>
-                                                <span class="fa fa-graduation-cap form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <select class="form-control has-feedback-left" name="horario">
-                                                    <option>Horário de Estudo</option>
-                                                </select>
-                                                <span class="fa fa-clock-o form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <select class="form-control has-feedback-left" name="instituicao_ensino">
-                                                    <option>Instituição de Ensino</option>
-                                                </select>
-                                                <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <input type="text" value="{{ $estagiario->matricula }}" class="form-control has-feedback-left" placeholder="Matricula" name="matricula">
-                                                <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <input type="text" value="{{ $estagiario->prev_termino }}" class="form-control has-feedback-left" placeholder="Previsão Término Curso" name="prev_termino">
-                                                <span class="fa fa-book form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <input type="text" value="{{ $estagiario->banco_agencia }}" class="form-control has-feedback-left" placeholder="Banco/Agência" name="banco_agencia">
-                                                <span class="fa fa-bank form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <input type="text" value="{{ $estagiario->conta }}" class="form-control has-feedback-left" placeholder="Conta" name="conta">
-                                                <span class="fa fa-bars form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <input type="text" value="{{ $estagiario->codigo_vaga }}" class="form-control has-feedback-left" placeholder="Código da vaga" name="codigo_vaga">
-                                                <span class="fa fa-barcode form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                                <input type="text" value="{{ $estagiario->criar_senha }}" class="form-control has-feedback-left" placeholder="Criar Senha" name="criar_senha">
-                                                <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
-                                            </div>
-                                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                                <div class="checkbox">
-                                                    <label>
-                                                    <input type="checkbox" class="flat"> Esta Ativo
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
-                                                <textarea class="form-control" placeholder="Sua observação" name="criar_senha">
-                                                </textarea>
-                                            </div>
-                                        </div>
-                                        <button  type="submit"class="btn btn-success" style="margin: 20px auto; display:block;">Salvar Alterações</button>
-                                    </div>
+                                        kkk
                                   </div>
-
                               </div>
+                              <div id="step-2">
+                                  <br>
+                                  <div id="form-step-1" role="form" data-toggle="validator">
+                                        kdk
+                                  </div>
+                              </div>
+                            </div>
                           </div>
                       </div>
-                     </div>
+                      @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
                       </form>
                   </div>
                 </div>
