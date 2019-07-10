16d5b62ff409a4bf4b07b35889564f189ce17504
WillamisRodrigues <wrodrigues153@gmail.com>
2019-07-10 01:18:31 -0300

    view

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -78,21 +78,6 @@
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>TCE ATIVOS </h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content2">
@@ -140,7 +125,6 @@
         @include('layout.footer')
       </div>
     </div>
-    <script src=https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.2/echarts-en.min.js charset=utf-8></script>
         {!! $chart->script() !!}
         {!! $chart2->script() !!}
         {!! $chart3->script() !!}
