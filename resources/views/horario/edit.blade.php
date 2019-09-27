8c674dace18140f5768ae0a43577994ad0837fc5
WillamisRodrigues <wrodrigues153@gmail.com>
2019-09-27 00:17:40 -0300

    views abas configurações

--- resources/views/horario/edit.blade.php
+++ resources/views/horario/edit.blade.php
@@ -1,1 +1,80 @@
-Editar horario
+@extends('layout/app')
+@section('titulo','Horários de Estágio - Trabalho - Editar - TCE | RH MAIS')
+@section('conteudo')
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+          @include('layout.menu.menu')
+            <!-- /menu profile quick info -->
+
+            <br />
+            @include('layout.menu.sidebar')
+            <!-- /sidebar menu -->
+          </div>
+        </div>
+        @include('layout.menu.menutop')
+        <!-- page content -->
+        <div class="right_col" role="main">
+            <div class="clearfix"></div>
+
+            <div class="row">
+
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Horários de Estágio - Trabalho - Editar</h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                  <form action="{{ route('horario.store') }}" method="post">
+                  {{csrf_field()}}
+
+                      <!-- SmartWizard html -->
+                      <div>
+                          <div>
+                              <div>
+                                    <div id="form-step-0" role="form" data-toggle="validator">
+                                      <div class="row" style="width:960px; margin: 20px auto;">
+                                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                              <input type="text" class="form-control has-feedback-left" placeholder="Descrição do Horário:" name="descricao">
+                                              <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                              <input type="text" class="form-control has-feedback-left" placeholder="Quantidade Horas:" name="horas">
+                                              <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                             <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <select class="form-control has-feedback-left" name="empresa_id">
+                                                    <option>Selecione Unidade Concedente:</option>
+                                                        <option value=""></option>
+                                                     
+                                                </select>
+                                                <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                          <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                                                <input type="text" class="form-control has-feedback-left" value="RH Mais" readonly placeholder="Agente de Integração" name="agente_int">
+                                                <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
+                                            </div>
+                                      </div>
+                                    <div class="btn-group mr-2 sw-btn-group-extra" role="group">
+                                      <button type="submit" class="btn btn-info">Enviar</button>
+                                      <button class="btn btn-danger">Cancelar</button>
+                                    </div>
+                              </div>
+                      </div>
+                     </div>
+                      </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+        <!-- footer content -->
+        @include('layout.footer')
+        <!-- /footer content -->
+      </div>
+    </div>
+@endsection
