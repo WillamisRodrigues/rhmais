8df7e3d883834a0adefc406ea0957c6b304aa918
Will Rodrigues <wrodrigues153@gmail.com>
2020-01-04 11:49:53 -0300

    ajuste folha

--- resources/views/pdf/folha/index.blade.php
+++ resources/views/pdf/folha/index.blade.php
@@ -1,481 +1,72 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Folha de pagamento</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Relatório de Valores - Bolsa Auxilio</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <style>
+        .tab,
+        tr {
+            border: 2px solid #999999;
+        }
+
+        body {
+            font-size: 1.05rem
+        }
+    </style>
 </head>
+
 <body>
-<h1> Folha de pagamento</h1><br>
-<h2>Relatório de Valores - Bolsa Auxílio</h2> <br>
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-22/05/2019 31/12/2019
-20190104021000 BIANCA CAROLINA DE OLIVEIRA 509.364.118-99
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-AV. COACIARA BL17 APT24
-PARQUE DOM PEDRO II
-1101
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-180.00 0.00 180.00
-22/08/2019 31/12/2020
-20190822051901 CHRISTIAN FELIPE RAIMUNDO 358.956.238-24
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA JOÃO CORREA CEZAR
-PQ VALENÇA II (19)3221-9359
-124
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-15/04/2019 31/12/2019
-20190411044947 CLEANE DA CONCEIÇÃO FERREIRA 524.058.758-29
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA TRISTA E UM DE MAIO
-JARDIM SANTA EUDOXIA
-30
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-19/06/2019 31/12/2019
-20190618055612 DHIULY DOS SANTOS VIEIRA 462.525.208-37
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-AVENIDA ANA MARIA MARQUES
-JARDIM MARIA ROSA (19)9898-2024
-325
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-240.00 0.00 240.00
-19/08/2019 19/08/2021
-20190816063801 EDUARDA SOUZA DA SILVA 367.035.908-77
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA DANILO TAVOLARO
-JARDIM NOVO CAMPOS ELISEOS
-205
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-15/04/2019 31/12/2020
-20190411062809 FELIPE MARQUES TESSARI DE OLIVEIRA 522.577.888-77
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA CARLOS GOBBI
-CJ RES PARQUE SÃO BENTO (19)3221-9417
-19
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-700.00 0.00 700.00
-07/01/2019 31/12/2019
-20190104011841 GABRIELA LIBÓRIO CASSIM 450.903.308-75
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-AV GENERAL CARNEIRO BLOCO A AP
-AV GENERAL CARNEIRO
-576
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-19/06/2019 31/12/2020
-20190618051405 HUGO LUIS ROSA DOS SANTOS 062.022.601-30
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA CAUIM
-PARQUE UNIVERSITÁRIO DE VIRACO (19)3043-6425
-195
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 20.00 580.00
-Santander
-16/04/2019 31/12/2019
-20190415062044 ISABELA FERNANDA SANT ANA LOPES 506.703.208-11
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA SARGENTO LUIZ MORAES CASA
-JARDIM SÃO VICENTE (19)9102-1983
-196
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-23/05/2019 31/12/2020
-20190522052905 JOÃO GUILHERME MORAIS 504.285.178-03
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA ROMILDO SEGALLIO
-JARDIM NILOPOLIS (19)3525-5404
-189
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Page 1 of 3Relatório de Valores - Bolsa Auxílio
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-28/01/2019 31/12/2019
-20190125063018 JOÃO MARCOS DE CASTRO FERREIRA 463.312.568-06
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA MARCELINA RODRIGUES PASCHOAL
-JARDIM DO LAGO
-310
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-19/06/2019 31/12/2019
-20190618063558 JOÃO PAULO DO NASCIMENTO SILVA 507.933.698-60
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-ESTAÇÃO MUNICIPAL JOSÉ SEDANO BL 3 AP 41
-TECHNO CAMPINAS
-40
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-11/03/2019 31/12/2020
-20190308035231 KAIKE PISSAROUK PEINADO 522.898.198-59
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA LUÍS OCTÁVIO TEIXEIRA MENDES
-DIC 3
-100
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-06/05/2019 31/12/2020
-20190503072514 KAUANA NATIELI DA SILVA MARTINS 483.634.118-33
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RENE PAULINO DA SILVA CASA
-RESIDENCIAL SÃO LUIZ
-18
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-09/04/2019 31/12/2019
-20190408081253 LETÍCIA DOS SANTOS ALVES 523.387.028-24
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA BERNADINO DE CAMPO APT 71
-CENTRO
-1.069
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-700.00 0.00 700.00
-07/01/2019 31/12/2019
-20190104023745 MARIA NAIZI SILVA DOS SANTOS 520.491.778-01
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-AVENIDA JÚLIO RAIMUNDO GRANJA VIELA 14
-JARDIM PARANAPANEMA (19)3294-0697
-114
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-29/07/2019 29/07/2021
-20190725042144 MICHEL NASCIMENTO DE CASTRO 508.311.858-05
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA JOÃO CARLOS NOUGUES CASA
-JARDIM DO LAGO
-1095
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-240.00 0.00 240.00
-19/08/2019 31/12/2020
-20190816015311 MIGUEL GUILHERME OLIVEIRA MARTINS DA SILVA 535.569.718-60
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA FERDINANDO TURQUETTI
-JARDIM DAS BANDEIRAS
-646
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-07/01/2019 31/12/2019
-20190104065227 NÁTALIE SCHREITER NUNES DA COSTA 512.764.508-42
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA MAESTRO LUIS DE TÚLIO
-VILA BRANDINA (19)3291-3639
-511
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-520.00 0.00 520.00
-05/08/2019 31/12/2020
-20190805060746 PEDRO AUGUSTO DOS SANTOS 359.853.328-45
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA JOÃO THEODORO DE OLIVEIRA
-JARDIM FLORENCE II
-159
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-0676
-11/03/2019 31/12/2019
-20190308044104 PEDRO HENRIQUE COSTA DO NASCIMENTO 362.818.178-00
-6655-7
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA ANTÔNIO GRIGOLETO ESCOLA
-JARDIM GARCIA (19)3267-2677
-504
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Page 2 of 3Relatório de Valores - Bolsa Auxílio
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-22/04/2019 31/12/2019
-20190418010219 REBEKA JULIA SANTOS PEREIRA 522.725.398-66
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA DOIS
-VILA SÃO LUIZ
-68
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-29/07/2019 31/12/2019
-20190725060024 VICTOR BRAGA NASCIMENTO 469.189.208-71
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-LUZIA DE MELI AGUIAR
-JD VISTA ALEGRE
-300
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-28/01/2019 31/12/2019
-20190125011741 VINÍCIUS NASCIMENTO DA COSTA 515.850.368-03
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA MARIA MARTA CAMILO DE ANDRADE CASA
-PARQUE FLORESTA II (19)3221-9080
-29
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-240.00 0.00 240.00
-19/08/2019 31/12/2019
-20190816060040 WILLIAM GABRIEL TURA 474.304.408-19
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-RUA SINESIO DE MELO E OLIVEIRA
-JARDIM GUARANI (19)3252-1860
-310
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-09/04/2019 31/12/2019
-20190408044146 YASMIN CAROLINE ELOI 532.969.538-41
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-AV. FREI ANTÔNIO DE SANT'ANA GALVÃO
-CJ RES PARQUE SÃO BENTO (19)3226-2520
-852
-CAMPINAS
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-Estagiário:
-Rua: Nº: Complemento:
-Cidade: Bairro: Telefone:
-Valor Créditos: Valor Débitos : Valor Líquido:
-C.P.F.:
-600.00 0.00 600.00
-22/04/2019 31/12/2019
-20190418034223 YASMIN VITÓRIA ALVES DA SILVA 512.668.928-26
-Referência: 2019/08
-Banco/Ag: C/C Tipo: Dt Início TCE: Dt Fim TCE:
-AVENIDA COMENDADOR EMÍLIO PIERI
-CAMPINAS AVENIDA COMENDADOR EMÍLIO PIER
-Valor Pcte: 0.00 Percentual ( % ) : 0.00 Valor: 0.00
-Unidade: R M SOLUÇÕES SERVIÇOS DE COBRANÇAS LTDA RM COBRANÇAS
-Custo Unitário : 80.00 Valor Adicional : 0.00
-0.00 0.00
-QUANTIDADE DE ESTAGIÁRIOS: 27 VALOR TOTAL LIQUIDO FOLHA: 14,800.00
-Total Custo Fixo: Total Custo Unitário: 2,160.00 Total Custo Adicional: 0.00 Valor Total % :
-16/09/2019 19:58
+<img src="https://www.rhmaistalentos.com.br/imagens/logo-site.png" alt="" style="margin:0 auto; disply:block; width:20%;">
+<h4 class="text-center">Relatório de Valores - Bolsa Auxilio</h4>
+<br>
+<div class="tab">
+    <table class="table" style="max-width: 100%">
+        <tr>
+            <td><strong> Matricula:</strong> 201901111333</td>
+            <td><strong> Estagiário: </strong> Handila Mirian </td>
+            <td><strong> CPF: </strong> 570.328.598-44</td>
+        </tr>
+        <tr>
+            <td><strong> Unidade:</strong> Vasconcelos Pires de Azevedo Cobrança ltda</td>
+            <td><strong> Rua: </strong> Professora Ruth </td>
+            <td><strong> Nº: </strong>  181</td>
+            <td><strong> Complemento: </strong>  ---</td>
+        </tr>
+        <tr>
+            <td><strong> Cidade:</strong> Campinas</td>
+            <td><strong> Bairro: </strong> jardim do lago </td>
+            <td><strong> Telefone: </strong>  --</td>
+        </tr>
+        <tr>
+            <td><strong> Valor Crédito:</strong> 541.67</td>
+            <td><strong> Valor Débito: </strong> 0.00 </td>
+            <td><strong> Valor Liquido: </strong> 541.67 </td>
+            <td><strong> Referência: </strong> 2019/11 </td>
+        </tr>
+        <tr>
+            <td><strong> Banco/A:</strong> </td>
+            <td><strong> CC/Tipo: </strong>  </td>
+            <td><strong> Dt Início TCE: </strong> 11/11/2019 </td>
+            <td><strong> Dt Fim TCE: </strong> 25/11/2019 </td>
+        </tr>
+        <tr>
+            <td><strong> Valor Pcte: 0.00</strong> </td>
+            <td><strong> Percentual %: </strong>  </td>
+            <td><strong> Valor: </strong> 0.00 </td>
+        </tr>
+        <tr>
+            <td><strong> Custo Unitário: 80.00</strong> </td>
+            <td><strong> Valor Adicional: </strong>  </td>
+        </tr>
+    </table>   
+</div> 
 </body>
+
 </html>
-
-
